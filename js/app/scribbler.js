5dc41cbaeef10aa7e4a2c05e69ebb0651d9280c2
Felix Palmer <felixpalmer@gmail.com>
2013-12-04 10:36:55 +0100

    Simple drawing code

--- js/app/scribbler.js
+++ js/app/scribbler.js
@@ -6,14 +6,40 @@ define( ["drawing-container"], function( container ) {
       scribbler.ctx = scribbler.canvas.getContext( '2d' );
       container.appendChild( scribbler.canvas );
 
+      // Update the canvas size when the window is resized
       window.addEventListener( 'resize', scribbler.updateSize, false );
       scribbler.updateSize();
+
+      // Listen for mouse events
+      scribbler.canvas.addEventListener( 'mousedown', scribbler.onMouseDown, false );
+      scribbler.canvas.addEventListener( 'mousemove', scribbler.onMouseMove, false );
+      scribbler.canvas.addEventListener( 'mouseup', scribbler.onMouseUp, false );
 
       scribbler.clear();
     },    
     clear: function() {
       scribbler.ctx.fillStyle = "white";
       scribbler.ctx.fillRect( 0, 0, scribbler.canvas.width, scribbler.canvas.height );
+    },
+    drawing: false,
+    onMouseDown: function( e ) {
+      scribbler.drawing = true;
+      scribbler.paint( e.offsetX, e.offsetY );
+    },
+    onMouseMove: function( e ) {
+      if ( scribbler.drawing ) {
+        scribbler.paint( e.offsetX, e.offsetY );
+      }
+    },
+    onMouseUp: function( e ) {
+      scribbler.drawing = false;
+    },
+    paint: function( x, y ) {
+      scribbler.ctx.beginPath();
+      scribbler.ctx.arc( x, y, 10, 0, 2 * Math.PI, false );
+      scribbler.ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+      scribbler.ctx.fill();
+      scribbler.ctx.closePath();
     },
     updateSize: function() {
       scribbler.canvas.width = container.offsetWidth;
