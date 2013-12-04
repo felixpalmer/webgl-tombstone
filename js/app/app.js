7a147ecea064f4ec48646c5278ef538428a8d7c8
Felix Palmer <felixpalmer@gmail.com>
2013-12-04 10:17:20 +0100

    Switch to stone texture

--- js/app/app.js
+++ js/app/app.js
@@ -1,16 +1,16 @@
 define( ["three", "camera", "controls", "geometry", "light", "material", "renderer", "scene", "scribbler", "texture" ], 
 function( three, camera, controls, geometry, light, material, renderer, scene, scribbler, texture ) {
   var app = {
-    mesh: new THREE.Mesh( geometry["cube"], material["grass"] ),
+    mesh: new THREE.Mesh( geometry["block"], material["stone1"] ),
     init: function() {
       scene.add( app.mesh );
+      app.mesh.rotation.x = Math.PI / 8;
       light.target = app.mesh;
     },
     animate: function() {
       requestAnimationFrame( app.animate );
       controls.update()
-      app.mesh.rotation.x += 0.005;
-      app.mesh.rotation.y += 0.01;
+      app.mesh.rotation.y += 0.006;
       renderer.render( scene, camera );
     }
   }
--- js/app/scribbler.js
+++ js/app/scribbler.js
@@ -6,18 +6,19 @@ define( ["drawing-container"], function( container ) {
       scribbler.ctx = scribbler.canvas.getContext( '2d' );
       container.appendChild( scribbler.canvas );
 
-      var updateSize = function() {
-        scribbler.canvas.width = container.offsetWidth;
-        scribbler.canvas.height = container.offsetHeight;
-      }
-      window.addEventListener( 'resize', updateSize, false );
-      updateSize();
+      window.addEventListener( 'resize', scribbler.updateSize, false );
+      scribbler.updateSize();
 
       scribbler.clear();
     },    
     clear: function() {
       scribbler.ctx.fillStyle = "white";
       scribbler.ctx.fillRect( 0, 0, scribbler.canvas.width, scribbler.canvas.height );
+    },
+    updateSize: function() {
+      scribbler.canvas.width = container.offsetWidth;
+      scribbler.canvas.height = container.offsetHeight;
+      scribbler.clear();
     }
   };
 
