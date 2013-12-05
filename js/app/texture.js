df014e53ce3cc3565d8a381bef9565beb1c0a21d
Felix Palmer <felixpalmer@gmail.com>
2013-12-05 16:22:01 +0100

    Randomly load rock

--- js/app/texture.js
+++ js/app/texture.js
@@ -2,8 +2,10 @@ define( ["three", "scribbler"], function( THREE, scribbler ) {
   var texturePath = "js/textures/";
   var scribblerTexture = new THREE.Texture( scribbler.canvas );
   scribblerTexture.needsUpdate = true;
+  // Randomly load stone texture
+  var stone = "stone" + Math.ceil(2 * Math.random()) + ".png";
   return {
-    stone1: THREE.ImageUtils.loadTexture( texturePath + "stone1.png" ),
+    stone1: THREE.ImageUtils.loadTexture( texturePath + stone ),
     scribbler: scribblerTexture,
   }
 } );
