dc3cdbbbb6547e867cb99ccd8cf368e886fb12b3
Felix Palmer <felixpalmer@gmail.com>
2013-12-05 15:59:27 +0100

    Do not premultiply alpha

--- js/app/texture.js
+++ js/app/texture.js
@@ -2,7 +2,6 @@ define( ["three", "scribbler"], function( THREE, scribbler ) {
   var texturePath = "js/textures/";
   var scribblerTexture = new THREE.Texture( scribbler.canvas );
   scribblerTexture.needsUpdate = true;
-  scribblerTexture.premultiplyAlpha = true;
   return {
     stone1: THREE.ImageUtils.loadTexture( texturePath + "stone1.png" ),
     scribbler: scribblerTexture,
