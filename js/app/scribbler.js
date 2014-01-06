ef4b84b22a4e3f30dd669c0ab95142090b9efb8d
Felix Palmer <felixpalmer@gmail.com>
2014-01-06 17:21:52 +0100

    Copy alpha channel into gradient image

--- js/app/scribbler.js
+++ js/app/scribbler.js
@@ -7,7 +7,10 @@ define( ["drawing-container"], function( container ) {
     init: function() {
       scribbler.canvas = document.createElement( 'canvas' );
       scribbler.ctx = scribbler.canvas.getContext( '2d' );
-      container.appendChild( scribbler.canvas );
+      scribbler.gradientCanvas = document.createElement( 'canvas' );
+      scribbler.gradientCtx = scribbler.gradientCanvas.getContext( '2d' );
+      //container.appendChild( scribbler.canvas );
+      container.appendChild( scribbler.gradientCanvas );
 
       // Update the canvas size when the window is resized
       window.addEventListener( 'resize', scribbler.updateSize, false );
@@ -21,13 +24,20 @@ define( ["drawing-container"], function( container ) {
       scribbler.clear();
     },
     clear: function() {
-      scribbler.ctx.clearRect(0, 0, scribbler.canvas.width, scribbler.canvas.height);
+      scribbler.ctx.clearRect( 0, 0, scribbler.canvas.width, scribbler.canvas.height );
+      scribbler.gradientCtx.clearRect( 0, 0, scribbler.gradientCanvas.width, scribbler.gradientCanvas.height );
       scribbler.updated = true;
     },
     loadImage: function( url ) {
       var img = new Image();
       img.onload = function () {
-        scribbler.ctx.drawImage(img, 0, 0, scribbler.canvas.width, scribbler.canvas.height);
+        scribbler.ctx.drawImage( img, 0, 0, scribbler.canvas.width, scribbler.canvas.height );
+        scribbler.computeGradient( {
+          x: 0,
+          y: 0,
+          width: scribbler.canvas.width,
+          height: scribbler.canvas.height
+        } );
         scribbler.updated = true;
       };
       img.src = url;
@@ -60,7 +70,26 @@ define( ["drawing-container"], function( container ) {
     updateSize: function() {
       scribbler.canvas.width = container.offsetWidth;
       scribbler.canvas.height = container.offsetHeight;
+      scribbler.gradientCanvas.width = container.offsetWidth;
+      scribbler.gradientCanvas.height = container.offsetHeight;
       scribbler.clear();
+    },
+    computeGradient: function( dirtyRect ) {
+      var inputPixels = scribbler.ctx.getImageData( dirtyRect.x, dirtyRect.y,
+                                                    dirtyRect.width, dirtyRect.height ).data;
+      var outputData = scribbler.gradientCtx.getImageData( dirtyRect.x, dirtyRect.y,
+                                                             dirtyRect.width, dirtyRect.height );
+      var outputPixels = outputData.data;
+      for ( var x = 0; x <= dirtyRect.width; x++ ) {
+        for ( var y = 0; y <= dirtyRect.height; y++ ) {
+          var i = 4 * ( x + y * dirtyRect.width );
+          // Copy the alpha channel into the R channel
+          outputPixels[i] = inputPixels[i+3];
+          outputPixels[i+3] = 255;
+        }
+      }
+
+      scribbler.gradientCtx.putImageData( outputData, dirtyRect.x, dirtyRect.y );
     }
   };
 
